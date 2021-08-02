3cdf6bc1e29dd9497f613948920b86f251d0d184
Felipe Maciel <felipemaciel@Air-de-Felipe.bbrouter>
2021-08-02 15:33:54 -0300

    Creates login page

--- src/pages/User/SignIn/index.js
+++ src/pages/User/SignIn/index.js
@@ -1,4 +1,15 @@
 import React from 'react';
+import {
+	Card,
+	Background,
+	LogoContainer,
+	FormContainer,
+	Logo,
+	FormLabel,
+	Form,
+	Input,
+	Button,
+} from './styles';
 // import { Form, Input } from '@rocketseat/unform';
 // import * as Yup from 'yup';
 // import { toast } from 'react-toastify';
@@ -40,113 +51,31 @@ function UserSignIn() {
 	// }
 
 	return (
-		<>
-			<div className="App">
-				<div id="page-top">
-					{/* <!-- Page Wrapper  --> */}
-					<div id="wrapper">
-						{/* <!--  Content Wrapper  --> */}
-						<div id="content-wrapper" className="d-flex flex-column">
-							{/* <!--  Main Content  --> */}
-							<div id="content">
-								{/* <!--  Begin Page Content  --> */}
-								<div className="container-fluid">
-									{/* <!--  Page Heading  --> */}
-									<h1 className="h3 mb-4 text-gray-800">Novo Administrador</h1>
-
-									<div className="row">
-										<div className="col-md-12 col-sm-12">
-											<div className="card shadow mb-4">
-												{/* <!-- Card Header - Dropdown --> */}
-												{/* <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                            <h6 className="m-0 font-weight-bold text-primary">Informações di Item de SIC</h6>
-                            <div className="dropdown no-arrow">
-                                <div className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i className="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                                </div>
-                                <div id="dropdown-photo-new" className="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                                    aria-labelledby="dropdownMenuLink" x-placement="bottom-end">
-                                    <div className="dropdown-header">Dropdown Header:</div>
-                                    <div className="dropdown-item" href="#">Action</div>
-                                    <div className="dropdown-item" href="#">Another action</div>
-                                    <div className="dropdown-divider"></div>
-                                    <div className="dropdown-item" href="#">Something else here</div>
-                                </div>
-                            </div>
-                        </div> */}
-
-												{/* <!-- Card Body --> */}
-												<div className="card-body">
-													{/* <Form schema={schema} onSubmit={handleSubmit}>
-														<div className="form-group">
-															<label htmlFor="name">Nome</label>
-															<Input
-																className="form-control"
-																id="name"
-																name="name"
-																placeholder="Seu Nome"
-															/>
-														</div>
-
-														<div className="form-group">
-															<label htmlFor="email">Email</label>
-															<Input
-																className="form-control"
-																id="email"
-																name="email"
-																type="email"
-																placeholder="Seu email"
-															/>
-														</div>
-														<div className="form-group">
-															<label htmlFor="titulo">Senha</label>
-															<Input
-																className="form-control"
-																name="password"
-																type="password"
-																placeholder="Sua senha"
-															/>
-														</div>
-
-														<div className="form-group">
-															<label htmlFor="titulo">
-																Confirmação de Senha
-															</label>
-															<Input
-																className="form-control"
-																name="password_confirmation"
-																type="password"
-																placeholder="Confirme sua senha"
-															/>
-														</div>
-
-														<button
-															type="submit"
-															className="btn btn-success btn-block"
-														>
-															Cadastrar Administrador
-														</button>
-													</Form> */}
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-								{/*  /.container-fluid  */}
-							</div>
-							{/* End of Main Content */}
-						</div>
-						{/* <!--  End of Content Wrapper  --> */}
-					</div>
-					{/* <!--  End of Page Wrapper  --> */}
-
-					{/* <!--  Scroll to Top Button --> */}
-					<a className="scroll-to-top rounded" href="#page-top">
-						<i className="fas fa-angle-up" />
-					</a>
-				</div>
-			</div>
-		</>
+		<Background>
+			<Card>
+				<LogoContainer>
+					<Logo />
+				</LogoContainer>
+				<FormContainer>
+					<FormLabel>Bem Vindo</FormLabel>
+					<Form action="/action_page.php">
+						<Input
+							type="text"
+							id="email"
+							name="fname"
+							placeholder="Digite seu email"
+						/>
+						<Input
+							type="text"
+							id="fassword"
+							name="lname"
+							placeholder="Digite a senha"
+						/>
+						<Button>Entrar</Button>
+					</Form>
+				</FormContainer>
+			</Card>
+		</Background>
 	);
 }
 
