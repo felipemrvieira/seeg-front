c11f22bf16ca18f1c16c66cfdda34fed2e78b9fc
Felipe Maciel <felipemaciel@Air-de-Felipe.bbrouter>
2021-08-02 20:55:27 -0300

    Renders data in table

--- src/pages/Solution/Index/index.js
+++ src/pages/Solution/Index/index.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import Container from 'react-bootstrap/Container';
+import BTable from 'react-bootstrap/Table';
+import { useTable } from 'react-table';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { Background } from './styles';
 
 import Card from '../../../components/Card';
@@ -7,16 +11,108 @@ import PageHeader from '../../../components/PageHeader';
 
 import Header from '../../../components/Header';
 
-function Solution() {
+function Table({ columns, data }) {
+	// Use the state and functions returned from useTable to build your UI
+	const { getTableProps, headerGroups, rows, prepareRow } = useTable({
+		columns,
+		data,
+	});
+
+	// Render the UI for your table
+	return (
+		<BTable striped bordered hover size="sm" {...getTableProps()}>
+			<thead>
+				{headerGroups.map((headerGroup) => (
+					<tr {...headerGroup.getHeaderGroupProps()}>
+						{headerGroup.headers.map((column) => (
+							<th {...column.getHeaderProps()}>{column.render('Header')}</th>
+						))}
+					</tr>
+				))}
+			</thead>
+			<tbody>
+				{rows.map((row) => {
+					prepareRow(row);
+					return (
+						<tr {...row.getRowProps()}>
+							{row.cells.map((cell) => (
+								<td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+							))}
+						</tr>
+					);
+				})}
+			</tbody>
+		</BTable>
+	);
+}
+
+export default function Solution() {
+	const [solutions, setSolutions] = useState([]);
+	async function loadSolutions() {
+		try {
+			// const response = await api.get('/solutions');
+			const response = [
+				{ id: '1', title: 'solucao 1' },
+				{ id: '2', title: 'solucao 2' },
+			];
+			setSolutions(response);
+			// console.tron.log(response.data);
+		} catch (err) {
+			// console.tron.log(err);
+		}
+	}
+
+	useEffect(() => {
+		loadSolutions();
+	}, []);
+
+	const data = useMemo(() => solutions, [solutions]);
+
+	const columns = React.useMemo(
+		() => [
+			{
+				Header: 'Nome',
+				accessor: 'title',
+			},
+			{
+				Header: '',
+				accessor: 'id',
+				Cell: (row) => (
+					<div>
+						<Link
+							className="btn btn-light btn-icon-split"
+							to={`/solutions/${row.value}`}
+						>
+							<span className="icon text-gray-600">
+								<i className="fas fa-arrow-right" />
+							</span>
+							<span className="text">Visualizar</span>
+						</Link>
+					</div>
+				),
+			},
+		],
+		[]
+	);
 	return (
 		<Background>
 			<Header />
 			<Container>
 				<PageHeader>Listar Soluções</PageHeader>
-				<Card>asd</Card>
+				<Card>
+					<Table columns={columns} data={data} />
+				</Card>
 			</Container>
 		</Background>
 	);
 }
 
-export default Solution;
+Table.propTypes = {
+	columns: PropTypes.element,
+	data: PropTypes.element,
+};
+
+Table.defaultProps = {
+	columns: [],
+	data: [],
+};
