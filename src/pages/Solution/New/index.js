c2fea833f9f6379bd56cf1aa29a9c189e8be985b
Felipe Maciel <felipemaciel@Air-de-Felipe.bbrouter>
2021-08-04 14:17:58 -0300

    ajusts form to upload file

--- src/pages/Solution/New/index.js
+++ src/pages/Solution/New/index.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
+import { toast } from 'react-toastify';
+// import { useHistory } from 'react-router-dom';
 import { Background, ButtonWrapper, Label, Input } from './styles';
-
 import Card from '../../../components/Card';
 import PageHeader from '../../../components/PageHeader';
-
 import Header from '../../../components/Header';
+import api from '../../../services/api';
 
 function Solution() {
+	const [file, setFile] = useState({});
+	// const history = useHistory();
+
+	function fileUpload(fileparam) {
+		const formData = new FormData();
+		formData.append('xlsx_file', fileparam);
+		const config = {
+			headers: {
+				'content-type': 'multipart/form-data',
+			},
+		};
+		return api.post(`/solutions`, formData, config);
+	}
+
+	function onFormSubmit(e) {
+		e.preventDefault();
+		console.log('submited');
+		fileUpload(file).then((response) => {
+			console.log(response.data);
+			toast.success('Informação cadastrada com sucesso!!');
+			// history.push('/solutions');
+		});
+	}
+
+	function onChange(e) {
+		setFile(e.target.files[0]);
+	}
+
 	return (
 		<Background>
 			<Header />
@@ -15,14 +44,20 @@ function Solution() {
 				<PageHeader>Cadastrar Soluções</PageHeader>
 				<Card>
 					<p>Selecione um arquivo no formato xlsx</p>
-					<ButtonWrapper>
-						<Label>Selecione o arquivo</Label>
-						<Input
-							type="file"
-							name="upload"
-							placeholder="Selecione o arquivo"
-						/>
-					</ButtonWrapper>
+					<form onSubmit={onFormSubmit}>
+						<ButtonWrapper>
+							<Label>Selecione o arquivo</Label>
+							<Input
+								type="file"
+								name="upload"
+								onChange={onChange}
+								placeholder="Selecione o arquivo"
+							/>
+						</ButtonWrapper>
+						<button className="btn btn-light mt-2" type="submit">
+							Upload
+						</button>
+					</form>
 				</Card>
 			</Container>
 		</Background>
